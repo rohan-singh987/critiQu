434b82bd19fe5caee61fed1e472e83c0087492a0
Rohan Singh <rohannsingh98765@gmail.com>
2022-03-14 00:01:51 +0530

    auth
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- criti_indietro/index.js
+++ criti_indietro/index.js
@@ -2,19 +2,25 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
+app.use(express.json());
 
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
 
 })
 .then(console.log("connected"))
-.catch((err) => {
-    console.log(err);
-});
+.catch((err) => 
+    console.log(err)
+);
+ 
+app.use("/critindietro/auth", authRoute);
 
 
 app.listen('5050', () => {
